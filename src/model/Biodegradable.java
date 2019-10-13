c784f66ae0ef0c781f0384e3f1da63f12c0c1777
Santiago Hurtado Solis <guapicito723@gmail.com>
2019-10-13 12:00:11 -0500

    gg

---
+++ src/model/Biodegradable.java
@@ -0,0 +1,14 @@
+package model;
+public class Biodegradable{
+	//attributes
+	private boolean composting;
+
+	//mothods
+	public Biodegradable(boolean composting){
+		this.composting = composting;
+	}
+	public boolean getComposting(){
+		return this.composting;
+	}
+
+}
